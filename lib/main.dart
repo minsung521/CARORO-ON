9579becad8c76c69a3df1a68fe0afa7af388d101
minsung <mingsung521@gmail.com>
2021-10-29 09:54:07 +0900

    routing finished really

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:caroro_on/app/routes/route.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 void main() {
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
+    initialRoute: Routes.Home,
+    getPages: Pages.routes,
   ));
 }
